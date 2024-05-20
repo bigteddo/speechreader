bcfb080c362943709ddb8a823c7dce46f77a7c1c
tcelestin <tcelestin@mindedge.com>
2024-05-20 01:38:44 -0400

    Add context about library usage in README
    The README file is updated to specify that the "speechreader" library is intended for projects running on a web browser. This additional information provides better clarifications to users regarding the appropriate use of the library.

--- tests/add.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const src_1 = require("../src");
-test('adds two numbers correctly', () => {
-    const result = (0, src_1.add)(2, 3);
-    expect(result).toBe(5);
-});
